6406e1b0d888bfb631922e53b501096e1ad833df
Kao Ethan <ethansnow2012@gmail.com>
2021-02-05 22:28:36 +0800

    readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ This is reactive programming library for Javascript.
 
 ## Quickstart
 - Via CDN: `<script src="https://cdn.jsdelivr.net/npm/recole"></script>`
+## Quickstart
+- [Demo](https://ethansnow2012.github.io/recole):
--- dist/recole.js
+++ dist/recole.js
@@ -4,7 +4,7 @@
     (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.recole = factory());
 }(this, (function () { 'use strict';
 
-    var recole = function(){
+    var recole = function () {
         this.symbol_reactive = Symbol();
         this._beforeMap = new WeakMap();
         //this.effMap = new WeakMap();
@@ -13,16 +13,17 @@
         this.currentEffect = null;
         console.log("init");
     };
-    recole.prototype.addBefore = function(target, before){
+    recole.prototype.addBefore = function (target, before) {
         let set = this._beforeMap.get(target);// return object of type "Set"
         if (!set) {
             this._beforeMap.set(target, (set = new Set()));
         }
         set.add(before);
     };
-    recole.prototype.track = function(target, key,options={}) {
-        
+    recole.prototype.track = function (target, key, options = {}) {
+        if(this.escape_effect_flag){ return }
         target = this._2targetObj(target);
+        
         if (this.currentEffect) {
             let depsMap = this.targetMap.get(target); // Get the current depsMap for this target
             if (!depsMap) {
@@ -34,32 +35,32 @@
                 // There is no dependencies (effects)
                 depsMap.set(key, (dep = new Set())); // Crecolee a new Set
             }
-            if(options.before){
+            if (options.before) {
                 this.addBefore(target, options.before);
             }
             //this.effMap.set(this.currentEffect, this.currentEffect)
             dep.add(this.currentEffect);
         }
     };
-    recole.prototype.cancel = function(target){
+    recole.prototype.cancel = function (target) {
         target = this._2targetObj(target);
         this._beforeMap.delete(target);
         this.targetMap.delete(target);
-        
+
     };
 
-    recole.prototype.trigger_before = function(target, key) {
+    recole.prototype.trigger_before = function (target, key) {
         target = this._2targetObj(target);
         //==
         let beforeSet = this._beforeMap.get(target);
-        if(beforeSet){
-            beforeSet.forEach((eff)=>{
+        if (beforeSet) {
+            beforeSet.forEach((eff) => {
                 eff();
             });
         }
     };
-    recole.prototype.trigger = function(target, key) {
-        
+    recole.prototype.trigger = function (target, key) {
+        if(this.escape_effect_flag){ return }
         target = this._2targetObj(target);
         //==
         const depsMap = this.targetMap.get(target); // Does this object have any properties that have dependencies (effects)
@@ -75,10 +76,12 @@
         }
     };
 
-    recole.prototype.createReactive = function (target)  {
+    recole.prototype.createReactive = function (target, _relation_obj=null,_cache_obj=null) {
         let _this = this;
+        let relation_obj = _relation_obj;
+        let cache_obj = _cache_obj;
         const handler = {
-            
+
             get(target, key, receiver) {
                 let result = Reflect.get(target, key, receiver);
                 _this.track(target, key); // If this reactive property (target) is GET inside then track the effect to rerun on SET
@@ -86,30 +89,45 @@
             },
             set(target, key, value, receiver) {
                 let oldValue = target[key];
-                if (oldValue != value) {
-                    _this.trigger_before(target, key); 
+                if ((oldValue != value)) {
+                    _this.trigger_before(target, key);
                 }
                 let result = Reflect.set(target, key, value, receiver);
-                if (result && oldValue != value) {
+                if (result && (oldValue != value)) {
                     _this.trigger(target, key); // If this reactive property (target) has effects to rerun on SET, trigger them.
                 }
                 return result
-            },
+            }
         };
         let _proxy = new Proxy(target, handler);
         let keys = Reflect.ownKeys(target);
-        keys.forEach((_key)=>{
-            let el =target[_key];
-            if(el 
-               && typeof el == "object"
-               && !Array.isArray(el)
-            ){
-                _this.effect(()=>{
+        keys.forEach((_key) => {
+            let el = target[_key];
+            if (el
+                && typeof el == "object"
+                && !Array.isArray(el)
+            ) {
+                _this.effect(() => {
                     _proxy[_key] = el.ref[el.key];//this.targetMap.get(el.ref)[el.key]
                 });
             }
         });
-        this.proxyMap.set(_proxy,target);
+        Object.setPrototypeOf(_proxy, {
+            set_relation_obj: (new_relation_obj)=>{
+                relation_obj = new_relation_obj;
+            },
+            set_cache_obj: (new_cache_obj)=>{
+                cache_obj = new_cache_obj;
+            },
+            relation_obj: ()=>{
+                return relation_obj
+            },
+            cache_obj:()=>{
+                return cache_obj
+            }
+        });
+        
+        this.proxyMap.set(_proxy, target);
         return _proxy
     };
     recole.prototype.effect = function (eff) {
@@ -118,17 +136,30 @@
         this.currentEffect = null;
         return eff
     };
-    recole.prototype.ref = function (init_value=0) {
+    recole.prototype.escape_effect = function (aa) {
+        let rtn;
+        this.escape_effect_flag = true;
+        if(typeof aa == "function"){
+            rtn = aa();
+        }else {
+            rtn = aa;
+        }
+        this.escape_effect_flag = null;
+        return rtn
+    };
+    recole.prototype.ref = function (init_value = 0, _relation_obj=null,_cache_obj=null) {
         let _this = this;
         let raw = init_value;
         let raw_old = init_value;
-        const r = {  
+        let relation_obj = _relation_obj;
+        let cache_obj = _cache_obj;
+        const r = {
             get value() {
                 _this.track(r, 'value');
-                return raw 
+                return raw
             },
             set value(newVal) {
-                if(raw == newVal){
+                if (raw == newVal) {
                     //raw_old = raw
                     return;
                 }
@@ -136,17 +167,30 @@
                 raw = newVal;
                 _this.trigger(r, 'value');
             },
-
             // failed design 
-            getOld: ()=>{ 
-                return raw_old 
+            getOld: () => {
+                return raw_old
             },
-            setOld: ()=>{
+            setOld: () => {
                 raw_old = raw;
-                return raw_old 
+                return raw_old
             }
             // failed design end
         };
+        Object.setPrototypeOf(r, {
+            set_relation_obj: (new_relation_obj)=>{
+                relation_obj = new_relation_obj;
+            },
+            set_cache_obj: (new_cache_obj)=>{
+                cache_obj = new_cache_obj;
+            },
+            relation_obj: ()=>{
+                return relation_obj
+            },
+            cache_obj:()=>{
+                return cache_obj
+            }
+        });
         return r
     };
     recole.prototype.computed = function (getter) {
@@ -154,23 +198,23 @@
         this.effect(() => (result.value = getter()));
         return result
     };
-    recole.prototype.watch = function(target, eff, before, options={}){
+    recole.prototype.watch = function (target, eff, before, options = {}) {
         //let _this = this
 
         this.currentEffect = eff;
-        if(options["first_effect"]==true){
+        if (options["first_effect"] == true) {
             eff();
         }
-        let trackOptions = before?{before:before}:{};
+        let trackOptions = before ? { before: before } : {};
         let keys = Reflect.ownKeys(target);
-        keys.forEach((_key)=>{
-            this.track(target,_key, trackOptions);
+        keys.forEach((_key) => {
+            this.track(target, _key, trackOptions);
         });
-        
+
         this.currentEffect = null;
     };
-    recole.prototype._2targetObj = function(target){
-        return this.proxyMap.get(target)?this.proxyMap.get(target):target
+    recole.prototype._2targetObj = function (target) {
+        return this.proxyMap.get(target) ? this.proxyMap.get(target) : target
     };
 
     return recole;
